0c868d27471904d2d5c22e54ee3d116e37758793
Jabbar Lamothe <prmtm@Jabbars-Mac-mini.local>
2022-03-15 23:19:42 -0500

    add login POST route

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 const mockUserData=[
-{name:'Mark'},
-{name:'Jill'}
+		{name:'Mark'},
+		{name:'Jill'}
 ]
 
-app.get('/users', function(req,res){
-		res.joint({
+app.get('/users',function(req,res){
+		res.json({
 				success: true,
 				message: 'successfully got users. Nice!',
 				users: mockUserData
@@ -21,6 +24,29 @@ app.get('/users/:id',function(req,res){
 				message: 'got one user',
 				user: req.params.id
 		})
+})
+
+app.post('/login',function(req,res){
+		const username+req.body.username;
+		const password+req.body.username;
+
+		const mockUsername="billyTheKid";
+		const mockPassword="superSecret";
+
+		if (username===mockUsername && password===mockPassword){
+			res.json({
+					success: true,
+					message: 'password and username match!',
+					token: 'encrypted token goes here'
+			})
+		} else {
+				res.json({
+					success: false,
+					message: 'password and username do not match'
+			})	
+		}
+
+
 })
 
 app.listen(8000,function(){console.log("server is running")})
