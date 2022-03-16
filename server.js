2671f09b3fed222820cbd02fe0cc056245279803
Jabbar Lamothe <prmtm@Jabbars-Mac-mini.local>
2022-03-15 23:58:38 -0500

    add login POST route

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 const mockUserData=[
 		{name:'Mark'},
@@ -16,7 +16,7 @@ app.get('/users',function(req,res){
 				users: mockUserData
 		})
 })
-
+// colons are used as variables that be viewed in the params
 app.get('/users/:id',function(req,res){
 		console.log(req.params.id)
 		res.json({
@@ -27,26 +27,27 @@ app.get('/users/:id',function(req,res){
 })
 
 app.post('/login',function(req,res){
-		const username+req.body.username;
-		const password+req.body.username;
+		
+		const username=req.body.username;
+		const password=req.body.password;
 
 		const mockUsername="billyTheKid";
 		const mockPassword="superSecret";
 
 		if (username===mockUsername && password===mockPassword){
-			res.json({
-					success: true,
-					message: 'password and username match!',
-					token: 'encrypted token goes here'
-			})
+				res.json({
+						success: true,
+						message: 'password and username match!',
+						token: 'encrypted token goes here'
+				})
 		} else {
 				res.json({
-					success: false,
-					message: 'password and username do not match'
-			})	
+						success: false,
+						message: 'password and username do not match'
+				})	
 		}
 
 
 })
 
-app.listen(8000,function(){console.log("server is running")})
+app.listen(8000,function(){console.log("server is listening")})
